20b267251ba42b5f2f1afc3d65b6ec5d690d43ee
tunglm <tunglm2203@kaist.ac.kr>
2020-09-28 16:48:35 +0900

    Moving the reward function into wrapper, add z-offset for minimum z-axis of gripper will be higher than table, add option using vision

--- rlbench/gym/rlbench_wrapper.py
+++ rlbench/gym/rlbench_wrapper.py
@@ -4,7 +4,7 @@ import rlbench.gym
 import numpy as np
 
 
-ENV_SUPPORT = ['reach_target-state-v0']
+ENV_SUPPORT = ['reach_target-state-v0', 'reach_target-vision-v0']
 
 
 # For action is joint space
@@ -86,12 +86,29 @@ class RLBenchWrapper_v1(core.Env):
     def __init__(self,
                  env_name,
                  render=False,
-                 action_scale=0.01):
+                 action_scale=0.1,
+                 height=128,
+                 width=128,
+                 min_z_offset=0.05):
         assert env_name in ENV_SUPPORT, 'Environment {} is not supported'.format(env_name)
 
         self._env_name = env_name
         self._render = render
         self._action_scale = action_scale
+        self._max_episode_steps = 100
+        self.height = height
+        self.width = width
+        self.min_z_offset = min_z_offset
+
+        if 'state' in env_name:
+            self._from_pixel = False
+        elif 'vision' in env_name:
+            self._from_pixel = True
+        else:
+            raise NotImplementedError('Not support this environment: {}'.format(env_name))
+
+        self.key_obs = 'front_rgb'
+
         # self._reset_tool_pos = np.array([0.125, 0., 1.])
         self._reset_tool_pos = np.array([0.25, 0., 1.])
         # Create task
@@ -116,6 +133,7 @@ class RLBenchWrapper_v1(core.Env):
 
         _boundary_center = self.env.task._task.boundaries.get_position()
         b_min_x, b_max_x, b_min_y, b_max_y, b_min_z, b_max_z = self.env.task._task.boundaries.get_bounding_box()
+        b_min_z += min_z_offset     # Add this offset to void gripper collide with table
         self.ws_min = np.round(_boundary_center + np.array([b_min_x, b_min_y, b_min_z]), 2)
         self.ws_max = np.round(_boundary_center + np.array([b_max_x, b_max_y, b_max_z]), 2)
 
@@ -151,8 +169,8 @@ class RLBenchWrapper_v1(core.Env):
                 continue
             if np.linalg.norm(del_pos) < 1e-3 and not done:
                 break
-        # Get low-dimension state after move to initial position
-        return self._get_obs(self._robot_state_low_dim)
+
+        return self._get_obs()
 
     def step(self, pos):
         action = self._make_full_action(pos, self._action_scale)
@@ -163,16 +181,19 @@ class RLBenchWrapper_v1(core.Env):
 
         rew = self.compute_reward(action, obs)
 
-        return self._get_obs(obs), rew, done, info
+        return self._get_obs(), rew, done, info
 
     def compute_reward(self, act, obs, info=None):
-        success, _ = self.env.task._task.success()
-        reward = self.env.task._task.reward()
+        if self.env.task._task.__class__.__name__ == 'ReachTarget':
+            success, _ = self.env.task._task.success()
+            reward = -np.linalg.norm(self.env.task._task.target.get_position() -
+                                     self.env.task._task.robot.arm.get_tip().get_position(), ord=2)
+            # final_rew = 0.0 if success else -1.0
+            total_rew = reward
+        else:
+            raise NotImplementedError('Not support compute reward for environment.')
 
-        # final_rew = reward + float(success)
-        # final_rew = 0.0 if success else -1.0
-        final_rew = reward
-        return final_rew
+        return total_rew
 
     def _make_full_action(self, pos, scale=1.0, check_valid=True):
         pos *= scale
@@ -184,9 +205,15 @@ class RLBenchWrapper_v1(core.Env):
         action_full = np.concatenate((pos, quat, gripper))
         return action_full
 
-    def _get_obs(self, obs_full):
-        # obs = np.concatenate((obs_full[7:14], obs_full[-3:]))
-        obs = np.concatenate((obs_full[22:25], obs_full[-3:]))
+    def _get_obs(self):
+        if self._from_pixel:
+            # Get visual state after move to initial position
+            obs = self.env.task._scene.get_observation()
+            obs = obs.__dict__[self.key_obs]
+        else:
+            obs_full = self._robot_state_low_dim
+            # obs = np.concatenate((obs_full[7:14], obs_full[-3:]))
+            obs = np.concatenate((obs_full[22:25], obs_full[-3:]))
         return obs.copy()
 
     def _check_workspace_valid(self, action):
